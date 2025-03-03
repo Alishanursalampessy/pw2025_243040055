f3d765331b5e80d4d32a87a4fbf5ea07bbf09a82
Alishanursalampessy <alishasalampessy@gmail.com>
2025-03-03 11:02:09 +0700

    selesai

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,12 @@
+<?php
+
+$nama = "Alisha Salampessy";
+$email = "alishasalampessy@gmail.com";
+$pekerjaan = "mahasiswa";
+$jurusan = "Teknik Informatika";
+$universitas = "UNPAS";
+
+echo "<h1>$nama</h1>";
+echo "<h4>$email</h4>";
+
+echo "<p>Halo, nama saya $nama saya seorang $pekerjaan di $jurusan $universitas</p>";
--- tugas/tugas3/3a.php
+++ tugas/tugas3/3a.php
@@ -1,29 +1,21 @@
 <?php
-
-$j = 10;
-$jr = 15;
-
-function luas_lingkaran($j)
+function hitungLuasLingkaran($r)
 {
-    return 3.14 * $j * $j;
+    $luas = pi() * pow($r, 2);
+    echo "<h4>Menghitung Luas Lingkaran</h4>";
+    echo "Jari-jari = $r cm.<br>";
+    echo "Luas lingkaran = " . round($luas, 1) . " cm²";
 }
 
-function keliling_lingkaran($r2)
+function hitungKelilingLingkaran($r)
 {
-    return 2 * 3.14 * $r2;
+    $keliling = 2 * pi() * $r;
+    echo "<h4>Menghitung Keliling Lingkaran</h4>";
+    echo "Jari-jari = $r cm.<br>";
+    echo "Keliling lingkaran = " . round($keliling, 1) . " cm";
 }
 
-
-echo "<h4>Menghitung Luas lingkaran</h4>";
-echo "<br>";
-echo "jari jari lingkaran = $j";
-echo "<br>";
-echo "Luas lingkaran = " . luas_lingkaran($j) . " cm<sup>2</sup>";
-echo "<br>";
+// Pemanggilan fungsi
+hitungLuasLingkaran(10);
 echo "<hr>";
-echo "<h4>Menghitung Keliling lingkaran</h4>";
-echo "<br>";
-echo "jari jari lingkaran = $jr";
-echo "<br>";
-echo "Keliling lingkaran = " . keliling_lingkaran($jr) . " cm";
-echo "<hr>";
+hitungKelilingLingkaran(20);
